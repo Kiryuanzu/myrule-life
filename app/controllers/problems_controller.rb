ede8c773283ad3b1f26409e7c7900554d6812d43
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-09-03 01:32:14 +0900

    Twitterログイン周りのルーティングとコントローラーの作成

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,2 +1,19 @@
 class ProblemsController < ApplicationController
+  def index
+  end
+
+  def new
+    @ploblem = Problem.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def problem_params
+  end
 end
