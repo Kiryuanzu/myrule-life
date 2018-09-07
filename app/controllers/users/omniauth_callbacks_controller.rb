0cf480c00150bd7c564ff2cc022f6d83dea16c16
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-09-07 23:53:19 +0900

    twitter認証のコントローラー周りを作った

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,2 +1,21 @@
-class Users::OmniauthCallbacksController < ApplicationController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController  
+  def twitter
+    collback_from :twitter
+  end
+
+  private
+
+  def callback_from(privider)
+    provider = provider.to_s
+
+    @uer = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
 end
